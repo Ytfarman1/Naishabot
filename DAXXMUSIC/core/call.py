9f23210997b9b23560099fe827415c830a9098ca
Ytfarman1 <farmanansari9698@gmail.com>
2025-09-03 19:50:05 +0530

    Update call.py

--- DAXXMUSIC/core/call.py
+++ DAXXMUSIC/core/call.py
@@ -48,7 +48,7 @@ async def _clear_(chat_id):
     await remove_active_chat(chat_id)
 
 
-class Call(PyTgCalls):
+class Call:
     def __init__(self):
         self.userbot1 = Client(
             name="DAXXAss1",
